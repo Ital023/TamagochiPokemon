bdf2d6e1cc4e96c6f2980067d9273c13cee1139b
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-09-15 11:08:23 -0300

    Adicionar arquivos de projeto.

---
+++ TamagochiPokemon/Program.cs
@@ -0,0 +1,13 @@
+﻿using RestSharp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var cliente = new RestClient("https://pokeapi.co/api/v2/pokemon/");
+        RestRequest request = new RestRequest("", Method.Get);
+        var response = cliente.Execute(request);
+
+        Console.WriteLine(response.Content);
+    }
+}
